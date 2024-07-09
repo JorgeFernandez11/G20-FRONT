4520aae962ca17e8273c9ff0752bc65ba709ddab
JorgeFernández11 <jorge.fernandezv01@gmail.com>
2024-07-08 23:53:48 -0500

    ref

---
+++ SEMANA 6/DIA 1/main.js
@@ -0,0 +1,44 @@
+// alert("Hola Mundo")
+// prompt("Ingresa datos")
+
+
+// Iniciacion con Js
+
+//Tipos de datos
+
+//Como se declara una variable
+
+// var numero = 15 casi no se usa por un ambito global -conflictos
+
+//Tipo de datos "Number"
+let numero1 = prompt("Ingresa el numero");  //let es para datos cambiantes
+let numero2 = prompt ("Ingresa el numero")
+let num1 = parseFloat(numero1);
+let num2 = parseFloat(numero2); //parsefloat lo convierte a numero , ya que inicialmente el prompt viene como string
+console.log(num1 * num2) 
+const pi = 3.14;   // const para variable fijas
+
+//Tipos de datos "String"
+let alumno = "Jorge"; 'Ignacio';
+
+//Tipo de datos "Booleano"
+let bool1 = true
+let bool2 = 1
+
+//Tipo de dato  "Null"
+let nada = null
+
+//Tipo de dato "Undefined"
+let noDefinido;
+
+//lo vemos en inspeccionar----->console
+// console.log("My name is" , alumno)
+// console.warn ("alerta")
+// console.error ("error")
+
+//alert ventana emergente
+// alert("alerta ventana emergente")
+// alert(3222)
+
+//prompt ventana que guarda datos
+// prompt("Ingresa tus datos")
